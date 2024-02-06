bd1ff8fe6ba7ee79689e1216acb6bed9c1418bd7
Sartaj Singh <sin14136@sheridancollege.ca>
2024-02-06 15:31:13 -0500

    branch part-time-student

---
+++ src/arraydemo/PartTimeStudent.java
@@ -0,0 +1,22 @@
+
+package arraydemo;
+
+/**
+ *
+ * @author sartaj
+ */
+public class PartTimeStudent extends Student  {
+    private int numCourses;
+
+    public PartTimeStudent(int id, String name) {
+        super(id, name);
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+}
